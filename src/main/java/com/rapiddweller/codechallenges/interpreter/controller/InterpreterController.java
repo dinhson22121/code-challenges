4e7fe83bb41eec14ffe638a6738160c62b490122
sngo6 <sondinh.ngo@dxc.com>
2024-01-25 18:24:00 +0700

    #3 - add new api translator

--- src/main/java/com/rapiddweller/codechallenges/interpreter/controller/InterpreterController.java
+++ src/main/java/com/rapiddweller/codechallenges/interpreter/controller/InterpreterController.java
@@ -7,19 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.script.ScriptException;
+
 @RestController
+@CrossOrigin(origins = "http://localhost:3000",maxAge = 3600)
 @RequestMapping("/api/v1")
 public class InterpreterController {
     @Autowired
     private InterpreterService interpreterService;
     @PostMapping("/arithmetic")
     public ResponseEntity<Response> simpleArithmetic(@RequestBody Interpreter code) throws Exception {
-        return ResponseEntity.ok(new Response(interpreterService.contextSubmissionAndExecution(code)));
+        return ResponseEntity.ok(new Response(interpreterService.simpleArithmetic(code)));
+    }
+
+    @PostMapping("/context-execution")
+    public ResponseEntity<Response> contextExecution(@RequestBody Interpreter code) throws Exception {
+        return ResponseEntity.ok(new Response(interpreterService.contextExecution(code)));
     }
 
     @PostMapping("/context-submission")
-    public ResponseEntity<Response> contextSubmissionAndExecution(@RequestBody Interpreter code) throws Exception {
-        return ResponseEntity.ok(new Response(interpreterService.contextSubmissionAndExecution(code)));
+    public ResponseEntity<?> contextSubmission(@RequestBody Interpreter code) throws Exception {
+        interpreterService.contextSubmission(code);
+        return ResponseEntity.ok(ResponseEntity.ok().body("Success"));
     }
     @PostMapping("/complex-operation")
     public ResponseEntity<Response> complexOperation(@RequestBody Interpreter code) throws Exception {
